caf6524b950fe8014ca762e20280fd27f3bb5810
Lukman Shaikh <shaikhlukman899@gmail.com>
2026-09-12 20:05:51 +0530

    my practice project

---
+++ index.js
@@ -0,0 +1,13 @@
+const themeBtn = document.querySelector("#themeBtn");
+
+themeBtn.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        themeBtn.textContent = "☀️";
+    } else {
+        themeBtn.textContent = "🌙";
+    }
+
+});
